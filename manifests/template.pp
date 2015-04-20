844f5f34ebe19b67409d0c9bf1a7bcb7b6281142
Vladislav Tkatchev <vlad.tkatchev@gmail.com>
2015-04-20 23:24:57 +0300

    fix

--- manifests/template.pp
+++ manifests/template.pp
@@ -1,10 +1,10 @@
 class zabbix::template (
-  template_name = undef,
-  template_source = undef,
+  templ_name = undef,
+  templ_source = undef,
   ) inherits zabbix::params {
 
   class { 'zabbix::resources::template':
-    template_name   => $template_name,
-    template_source => $template_source,
+    template_name   => $templ_name,
+    template_source => $templ_source,
   }
 }
