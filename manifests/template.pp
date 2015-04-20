fd9f92952eb7982c791b39874a633815aac04d1d
Vladislav Tkatchev <vlad.tkatchev@gmail.com>
2015-04-20 22:45:12 +0300

    fix

--- manifests/template.pp
+++ manifests/template.pp
@@ -2,9 +2,8 @@ class zabbix::template (
   
   ) inherits zabbix::params {
 
-    class { 'zabbix::resources::template':
-      template_name   => $template_name,
-      template_source => $template_source,
-    }
+  class { 'zabbix::resources::template':
+    template_name   => $template_name,
+    template_source => $template_source,
   }
 }
