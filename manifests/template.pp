a2ca45b2be3018ec66174f7cd0f2cc8482e97ec0
Vladislav Tkatchev <vlad.tkatchev@gmail.com>
2015-04-23 15:23:30 +0300

    defined type

--- manifests/template.pp
+++ manifests/template.pp
@@ -3,7 +3,7 @@ define zabbix::template (
   $templ_source = undef,
 ) {
 
-  zabbix::resources::template { "${template_name}":
+  zabbix::resources::template { "${templ_name}":
     template_name   => $templ_name,
     template_source => $templ_source,
   }
