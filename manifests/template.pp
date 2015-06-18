3a013f30c1fae7dcdab97d2e37fc68f0cc196c63
Vladislav Tkatchev <vlad.tkatchev@gmail.com>
2015-06-18 19:20:12 +0300

    syntax

--- manifests/template.pp
+++ manifests/template.pp
@@ -29,7 +29,7 @@
 define zabbix::template (
   $templ_name            = $title,
   $templ_source          = '',
-  )
+  ) {
   
   zabbix::resources::template { $templ_name:
     template_name   => $templ_name,
